52183d898eaea035b077c8bd359a89473e75a0a1
LilitKhachikyan <65469127+LilitKhachikyan@users.noreply.github.com>
2021-02-15 03:09:09 -0800

    Create templateMatchUpdate.py

---
+++ templateMatchUpdate.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+src = cv2.imread('src.jpeg')
+src_gray = cv2.imread('src.jpeg', 0)
+temp = cv2.imread('temp.jpeg', 0)
+match = cv2.matchTemplate(src_gray, temp, cv2.TM_CCOEFF_NORMED)
+best_match=[]
+for i in range(0, match.shape[0]-1):
+    for j in range(0, match.shape[1]-1):
+        if match[i][j]>0.49:
+            best_match.append((i,j))
+
+print(best_match)
+weight, height = temp.shape[::-1]
+
+for start in best_match:
+    cv2.rectangle(src, (start[1], start[0]), (start[1]+weight, start[0]+height), (29, 8, 1), 1)
+
+cv2.imshow('Source picture', src)
+cv2.waitKey()
+cv2.destroyAllWindows()
